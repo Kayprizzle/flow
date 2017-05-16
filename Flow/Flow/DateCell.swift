4347a75f2b484118f1e4c23a89de84fdc7c99f9e
Kayprizzle <kaypree.hodges@gmail.com>
2017-05-16 18:14:31 -0400

    Import JTAppleCalendar

---
+++ Flow/Flow/DateCell.swift
@@ -0,0 +1,15 @@
+//
+//  DateCell.swift
+//  Flow
+//
+//  Created by Kaypree Hodges on 5/15/17.
+//  Copyright © 2017 Kaypree Hodges. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class DateCell: JTAppleCell {
+
+    
+}
